bb7d4909b53acd88ce7773053b35eee95bbea7a7
Thomas <thomas@lekoala.be>
2017-05-11 12:23:55 +0200

    more flexible src setting

--- code/ChosenField.php
+++ code/ChosenField.php
@@ -7,12 +7,13 @@
  */
 class ChosenField extends ListboxField
 {
-
     protected $no_results_text;
     protected $allow_single_deselect = true;
     protected $allow_max_selected;
     protected $use_order = false;
     protected $disable_search = null;
+
+    private static $throw_exceptions = false;
 
     public function __construct($name, $title = null, $source = array(), $value = '', $form = null, $emptyString = null)
     {
@@ -67,6 +68,51 @@ class ChosenField extends ListboxField
         $this->setAttribute('data-chosen', json_encode($opts));
         Requirements::javascript(CHOSENFIELD_DIR . '/javascript/ChosenField.js');
         return parent::Field($properties);
+    }
+
+    public function setValue($val, $obj = null)
+    {
+        // If we're not passed a value directly,
+        // we can look for it in a relation method on the object passed as a second arg
+        if (!$val && $obj && $obj instanceof DataObject && $obj->hasMethod($this->name)) {
+            $funcName = $this->name;
+            $val = array_values($obj->$funcName()->getIDList());
+        }
+
+        if ($val) {
+            if (!$this->multiple && is_array($val)) {
+                throw new InvalidArgumentException('Array values are not allowed (when multiple=false).');
+            }
+
+            if ($this->multiple) {
+                $parts = (is_array($val)) ? $val : preg_split("/ *, */", trim($val));
+                if (ArrayLib::is_associative($parts)) {
+                    // This is due to the possibility of accidentally passing an array of values (as keys) and titles (as values) when only the keys were intended to be saved.
+                    throw new InvalidArgumentException('Associative arrays are not allowed as values (when multiple=true), only indexed arrays.');
+                }
+
+                // Doesn't check against unknown values in order to allow for less rigid data handling.
+                // They're silently ignored and overwritten the next time the field is saved.
+                parent::setValue($parts);
+            } else {
+                if (!in_array($val, array_keys($this->getSource()))) {
+                    if (self::config()->throw_exceptions) {
+                        throw new InvalidArgumentException(sprintf('Invalid value "%s" for multiple=false', Convert::raw2xml($val)));
+                    } else {
+                        $src = $this->getSourceAsArray();
+                        $src[$val] = $val;
+                        $this->setSource($src);
+                    }
+                }
+
+                $this->value = $val;
+            }
+        } else {
+            $this->value = $val;
+        }
+
+        return $this;
+        parent::setValue($value);
     }
 
     public function getPlaceholder()
